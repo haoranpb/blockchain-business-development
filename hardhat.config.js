7bdb018cec841ca85ab669f7843f42b776c6705f
Haoran <ludanxer@icloud.com>
2021-11-02 15:20:41 +0100

    rename API_URL

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@
 require('dotenv').config()
 require('@nomiclabs/hardhat-ethers')
 require('@nomiclabs/hardhat-etherscan')
-const { API_URL, PRIVATE_KEY, ETHERSCAN_KEY } = process.env
+const { ALCHEMY_KEY, PRIVATE_KEY, ETHERSCAN_KEY } = process.env
 
 module.exports = {
   solidity: '0.8.0',
@@ -12,7 +12,7 @@ module.exports = {
   networks: {
     hardhat: {},
     ropsten: {
-      url: API_URL,
+      url: ALCHEMY_KEY,
       accounts: [`0x${PRIVATE_KEY}`],
     },
   },
