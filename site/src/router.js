7cd6d05621412146e36664d86476dff41f68b0d1
Haoran <ludanxer@icloud.com>
2021-11-02 21:08:35 +0100

    move to vue-cli

--- site/src/router.js
+++ site/src/router.js
@@ -4,6 +4,6 @@ import Home from '@/views/HomePage.vue'
 const routes = [{ path: '/', component: Home }]
 
 export default createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 })
---
+++ site/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: { content: ['./public/**/*.html', './src/**/*.{js,vue}'] },
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
