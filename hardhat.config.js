080a8da51518b2169db8642611b64dcd42dad33e
Haoran <ludanxer@icloud.com>
2021-11-02 22:12:47 +0100

    rename env var for vue-cli

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,8 @@
 require('dotenv').config()
 require('@nomiclabs/hardhat-ethers')
 require('@nomiclabs/hardhat-etherscan')
-const { ALCHEMY_KEY, PRIVATE_KEY, ETHERSCAN_KEY } = process.env
+const { VUE_APP_ALCHEMY_KEY, VUE_APP_PRIVATE_KEY, VUE_APP_ETHERSCAN_KEY } =
+  process.env
 
 module.exports = {
   solidity: '0.8.0',
@@ -12,11 +13,11 @@ module.exports = {
   networks: {
     hardhat: {},
     ropsten: {
-      url: ALCHEMY_KEY,
-      accounts: [`0x${PRIVATE_KEY}`],
+      url: VUE_APP_ALCHEMY_KEY,
+      accounts: [`0x${VUE_APP_PRIVATE_KEY}`],
     },
   },
   etherscan: {
-    apiKey: ETHERSCAN_KEY,
+    apiKey: VUE_APP_ETHERSCAN_KEY,
   },
 }
