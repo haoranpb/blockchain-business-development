8b87527452745b1f965c9cb290f004183980a46f
Haoran <ludanxer@icloud.com>
2021-11-06 11:13:09 +0100

    refine house card for all senarios

--- site/src/components/HouseCard.vue
+++ site/src/components/HouseCard.vue
@@ -1,20 +1,46 @@
 <template>
   <div
-    class="w-48 h-64 bg-white px-4 py-2 rounded-md shadow-lg flex-none mb-10"
+    class="w-96 h-80 bg-white px-6 py-4 rounded-md shadow-lg flex-none mb-10"
   >
     <h3 class="font-medium text-lg">{{ house.physicalAddr }}</h3>
     <p class="text-gray-400 text-xs">{{ house.ID }}</p>
-    <div v-if="!house.active" class="flex flex-col space-y-4 mt-6">
+    <div v-if="!house.active && !OnGoing" class="flex flex-col space-y-4 mt-6">
       <VfInput v-model="price" placeholder="Price per day" />
       <VfInput v-model="colateral" placeholder="Colateral" />
       <VfButton @click="submit">Active</VfButton>
     </div>
     <div v-else-if="house.offer" class="flex flex-col space-y-2 mt-4">
+      <p>
+        Tenant:
+        <OutboundLink class="text-xs text-primary-600" :href="tenantLink">
+          {{ house.tennat }}
+        </OutboundLink>
+      </p>
       <p>Price per day: {{ house.price }}</p>
       <p>Colateral: {{ house.colateral }}</p>
       <p>Start Date: {{ startDate }}</p>
       <p>Renting Period: {{ rentingPeriod }}</p>
       <VfButton @click="accept">Accept Offer</VfButton>
+    </div>
+    <div v-else-if="OnGoing" class="flex flex-col space-y-4 mt-8">
+      <div v-if="house.owner == $store.state.user.address">
+        <p>
+          Rented out to:
+          <OutboundLink class="text-xs text-primary-600" :href="tenantLink">
+            {{ house.tennat }}
+          </OutboundLink>
+        </p>
+        <p>From {{ startDate }} for {{ rentingPeriod }} days</p>
+      </div>
+      <div v-else>
+        <p>
+          Rented from:
+          <OutboundLink class="text-xs text-primary-600" :href="ownerLink">
+            {{ house.owner }}
+          </OutboundLink>
+        </p>
+        <p>From {{ startDate }} for {{ rentingPeriod }} days</p>
+      </div>
     </div>
     <div v-else class="flex flex-col space-y-4 mt-8">
       <p>No offer yet</p>
@@ -27,11 +53,13 @@
 <script>
 import VfInput from '@/components/VfInput.vue'
 import VfButton from '@/components/VfButton.vue'
+import OutboundLink from '@/components/OutboundLink.vue'
 
 export default {
   components: {
     VfInput,
     VfButton,
+    OutboundLink,
   },
   props: {
     house: {
@@ -54,6 +82,15 @@ export default {
     rentingPeriod() {
       return Math.floor(this.house.rentingPeriod / 1000 / 60 / 60 / 24)
     },
+    tenantLink() {
+      return `https://ropsten.etherscan.io/address/${this.house.tennat}`
+    },
+    ownerLink() {
+      return `https://ropsten.etherscan.io/address/${this.house.owner}`
+    },
+    OnGoing() {
+      return !this.house.offer && !this.house.active && this.house.startDate > 0
+    },
   },
   methods: {
     submit() {
@@ -72,6 +109,27 @@ export default {
         })
         .then(() => {
           alert('Successfully Activated!')
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    accept() {
+      window.ethereum
+        .request({
+          method: 'eth_sendTransaction',
+          params: [
+            {
+              from: this.$store.state.user.address,
+              to: process.env.VUE_APP_CONTRACT_ADDRESS,
+              data: this.$contract.methods
+                .acceptOffer(this.house.ID)
+                .encodeABI(),
+            },
+          ],
+        })
+        .then(() => {
+          alert('Successfully Accepted!')
         })
         .catch((err) => {
           console.log(err)
