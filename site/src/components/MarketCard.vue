aae7e5b958a39bf1044c8fa858b70531a2db8796
Haoran <ludanxer@icloud.com>
2021-11-06 19:23:45 +0100

    refine marketplace

--- site/src/components/MarketCard.vue
+++ site/src/components/MarketCard.vue
@@ -1,13 +1,35 @@
 <template>
   <div
-    class="w-48 h-64 bg-white px-4 py-2 rounded-md shadow-lg flex-none mb-10"
+    class="w-90 h-64 bg-white px-4 py-2 rounded-md shadow-lg flex-none mb-10"
   >
     <h3 class="font-medium text-lg">{{ house.physicalAddr }}</h3>
     <p class="text-gray-400 text-xs">{{ house.ID }}</p>
-    <div class="flex flex-col space-y-4 mt-6">
-      <VfInput v-model="startDate" type="date" />
-      <VfInput v-model="rentingPeriod" placeholder="3 Days" type="number" />
-      <VfButton class="float-right" @click="submit">Make Offer</VfButton>
+    <div class="flex flex-row">
+      <div
+        class="
+          flex flex-col
+          space-y-2
+          mt-8
+          flex-none
+          border-r border-gray-200
+          pr-6
+          mr-6
+        "
+      >
+        <p>
+          Owner:
+          <OutboundLink class="text-sm text-primary-600" :href="ownerLink">
+            {{ house.owner.slice(0, 6) }}...
+          </OutboundLink>
+        </p>
+        <p>Price: {{ house.price }}</p>
+        <p>Colateral: {{ house.colateral }}</p>
+      </div>
+      <div class="flex flex-col space-y-4 mt-6 w-40">
+        <VfInput v-model="startDate" type="date" />
+        <VfInput v-model="rentingPeriod" placeholder="3 Days" type="number" />
+        <VfButton class="float-right" @click="submit">Make Offer</VfButton>
+      </div>
     </div>
   </div>
 </template>
@@ -15,11 +37,13 @@
 <script>
 import VfInput from '@/components/VfInput.vue'
 import VfButton from '@/components/VfButton.vue'
+import OutboundLink from '@/components/OutboundLink.vue'
 
 export default {
   components: {
     VfInput,
     VfButton,
+    OutboundLink,
   },
   props: {
     house: {
@@ -32,6 +56,11 @@ export default {
       startDate: null,
       rentingPeriod: null,
     }
+  },
+  computed: {
+    ownerLink() {
+      return `https://ropsten.etherscan.io/address/${this.house.owner}`
+    },
   },
   methods: {
     submit() {
